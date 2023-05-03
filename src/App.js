cd528a8476e82cb893286f2e7e1187b2507cfaf2
eliazzo <bethazz@hotmail.co.uk>
2023-05-03 15:31:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import AppHeader from './components/AppHeader';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader>Checklit</AppHeader>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AppHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function AppHeader() {
+  return (
+    <div>
+      <h1 className='text-l'>CheckLit</h1>
+      <h2 className='text-m'>Unlock your productivity potential</h2>
+      <img src='./logo.jpeg' alt='CheckLit logo'/>
+    </div>
+  )
+}
+
+export default AppHeader
