a023ba91e8c2cc1fa38b03340346a8aa446d108d
eliazzo <bethazz@hotmail.co.uk>
2023-05-04 17:39:10 +0100

    HandleClick removes 'display-none' class to make new list form visible on click

--- src/components/NewList.js
+++ src/components/NewList.js
@@ -2,22 +2,19 @@ import React from 'react'
 import { useState } from 'react';
 
 
-  
-
-
-
 function NewList() {
-  const [isActive, setIsActive] = useState(false); 
+  const [isActive, setIsActive] = useState(false); // isActive = current state value, setIsActive = function that updates the state
   const handleClick = event => {
-    setIsActive(current => !current);
+    setIsActive(!isActive);; ///setIsActive updates the value of isActive false => true (and visa versa)
   }
   return (
+    // User interface
     <div>
-        <h1>New List</h1>
-        <button className={isActive ? 'blue' : ''} onClick={handleClick}>+</button>
-        <div className='blue flex-col width display-none'>
-          <input type='text'></input>
-          <button className='top' type='submit'></button>
+        <h1 className='text-m '>New List</h1>
+        <button onClick={handleClick}>+</button> 
+        <div className={`newListForm flex-col width ${isActive ? '' : 'display-none'}`}>
+          <input className='width top-l' type='text'></input>
+          <button className='top-s width' type='submit'></button>
         </div>
     </div>
   )
