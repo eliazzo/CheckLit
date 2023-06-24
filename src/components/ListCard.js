9d6726c09762a5adc7fe03c382dbd9edc36a62b0
eliazzo <bethazz@hotmail.co.uk>
2023-06-24 17:31:34 +0100

    set up prettier with husky

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import React from "react";
 
 function ListCard() {
   return (
     <div>
-        <h1 className="text-m">My Lists</h1>
-        <div className='blue card-width'>
-            <h2>Shopping</h2>
-            <ul className='list-none'>
-                <li><input type='checkbox'></input>Eggs</li>
-                <li><input type='checkbox'></input>Bread</li>
-                <li><input type='checkbox'></input>Cheese</li>
-            </ul>
-        </div>
+      <h1 className="text-m">My Lists</h1>
+      <div className="blue card-width">
+        <h2>Shopping</h2>
+        <ul className="list-none">
+          <li>
+            <input type="checkbox"></input>Eggs
+          </li>
+          <li>
+            <input type="checkbox"></input>Bread
+          </li>
+          <li>
+            <input type="checkbox"></input>Cheese
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ListCard
+export default ListCard;
--- src/components/ViewForm.js
+++ src/components/ViewForm.js
@@ -1,27 +1,26 @@
 import React, { useEffect } from "react";
-import { useState } from 'react';
+import { useState } from "react";
 import ListCard from "./ListCard";
 
 export default function ViewForm() {
-
   const [listArr, setListArr] = useState([]);
 
-
- const addNewList = () => {
-  const newList = <ListCard/>
-  setListArr(prevArr => [...[prevArr], newList])
- }
+  const addNewList = () => {
+    const newList = <ListCard />;
+    setListArr((prevArr) => [...[prevArr], newList]);
+  };
 
   return (
     <div>
-    <h1 className="text-m ">New List</h1>
+      <h1 className="text-m ">New List</h1>
       <input className="width top-l" type="text"></input>
-      <button className='top-s width' type='submit' onClick={addNewList}>Add new list</button>
+      <button className="top-s width" type="submit" onClick={addNewList}>
+        Add new list
+      </button>
 
-      {listArr.map((list, index) => (<div key={index}>{list}</div>
+      {listArr.map((list, index) => (
+        <div key={index}>{list}</div>
       ))}
     </div>
-
   );
 }
-
