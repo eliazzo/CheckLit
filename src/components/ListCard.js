e699201da9561c3fd2e3ee98b58b9713e896185f
eliazzo <bethazz@hotmail.co.uk>
2023-06-26 10:17:02 +0100

    add item to list - incomplete

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,40 +1,41 @@
 import React from "react";
 import { useState } from "react";
 
-const ListItem = ({item}) => {
+const ListItem = ({ item }) => {
   return (
     <>
-    <input type="checkbox"></input>${item}
+      <input type="checkbox"></input>${item}
     </>
-    )
-}
+  );
+};
 const ListCard = ({ listName }) => {
-
-  const [listItemInput, setListItemInput] = useState("")
+  const [listItemInput, setListItemInput] = useState("");
   const handleInput = (event) => {
-    setListItemInput(event.target.value)
-    console.log(listItemInput)
-  }
+    setListItemInput(event.target.value);
+    console.log(listItemInput);
+  };
 
-  const [listItemArr, setListItemArr] = useState([])
+  const [listItemArr, setListItemArr] = useState([]);
   const addListItem = () => {
-    const newListItem = <ListItem/>
-    setListItemArr((prevList) => [...[prevList], newListItem])
-  }
+    const newListItem = <ListItem />;
+    setListItemArr((prevList) => [...[prevList], newListItem]);
+  };
   return (
     <div>
       <div className="blue card-width">
         <h2>{listName}</h2>
         <input type="text" onChange={handleInput}></input>
-        <button type="submit" onClick={addListItem}>Submit</button>
+        <button type="submit" onClick={addListItem}>
+          Submit
+        </button>
         <ul className="list-none">
           {listItemArr.map((item, index) => (
-            <li key={index}>{item}</li>))}
+            <li key={index}>{item}</li>
+          ))}
         </ul>
       </div>
     </div>
   );
-}
-
+};
 
 export default ListCard;
--- src/components/ViewForm.js
+++ src/components/ViewForm.js
@@ -6,19 +6,23 @@ export default function ViewForm() {
   const [listName, setListName] = useState("");
   const handleInput = (event) => {
     setListName(event.target.value);
-  }
+  };
 
   const [listArr, setListArr] = useState([]);
   const addNewList = () => {
-    const newList = <ListCard listName={listName}/>;
+    const newList = <ListCard listName={listName} />;
     setListArr((prevArr) => [...[prevArr], newList]);
   };
-  
 
   return (
     <div>
       <h1 className="text-m ">New List</h1>
-      <input className="width top-l" type="text" value={listName} onChange={handleInput}></input>
+      <input
+        className="width top-l"
+        type="text"
+        value={listName}
+        onChange={handleInput}
+      ></input>
       <button className="top-s width" type="submit" onClick={addNewList}>
         Add new list
       </button>
